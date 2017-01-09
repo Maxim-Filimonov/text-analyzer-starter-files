d3da5567238110281624467545a6a8a62b00e4e2
Maxim Filimonov <tpaktopsp@gmail.com>
2017-01-09 21:30:14 +0000

    Implement word count

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // your code here!
+function countWords(input) {
+    return input.split(' ').length;
+}
+
+$("form").submit(function(e) {
+    e.preventDefault();
+    $(".hidden").removeClass("hidden");
+    var input = $("#user-text").val();
+    var count = countWords(input);
+    $("#word-count-label").text(count);
+});
